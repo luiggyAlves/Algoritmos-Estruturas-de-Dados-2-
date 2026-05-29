06a5a03a415a32b339147ad80ff4c6ef84ddabaa
luiggy <luiggy.alves.dev@gmail.com>
2026-05-29 15:02:02 -0400

    Implementação das funções comuns à ABP e AVL

--- AVL/AVL.c
+++ AVL/AVL.c
@@ -10,3 +10,74 @@ struct NO{
     struct NO* esq;
     struct NO* dir;
 };
+
+// Com exceção da inserção e remoção,onde o balanceamento ocorre, as outras funções são implementadas de forma identica à da ABP
+ArvAVL* cria_ArvAVL()(){
+    ArvBin* raiz = (ArvBin*) malloc(sizeof(ArvBin)); 
+    if(raiz) *raiz = NULL; 
+    return raiz;
+}
+
+void libera_NO(struct NO* no){
+    if(!no) return; 
+    libera_NO(no->esq);
+    libera_NO(no->dir);
+    free(no);
+    no = NULL;
+}
+void libera_ArvAVL(ArvAVL* raiz){
+    if(!raiz) return;
+    libera_NO(*raiz);
+    free(raiz);
+}
+
+int consultaArvAVL(ArvAVL* raiz, int valorBuscado){
+    if(!raiz) return 0; // Não dá pra buscar numa árvore vazia
+    struct NO* atual = *raiz;
+    while(atual){
+            if(valorBuscado==atual->info) return 1;
+            else if(valorBuscado > atual->info) atual = atual->dir;
+            else atual = atual->esq;
+    }
+    return 0;
+    }
+
+int estaVazia_ArvAVL(ArvAVL* raiz){
+    if(!raiz) return 1; 
+    if(!(*raiz)) return 1; 
+    return 0;  
+}
+
+int totalNO_ArvBin(ArvBin *raiz){
+    if(!raiz || !(*raiz)) return 0;
+    int total_esq = totalNO_ArvBin(&((*raiz)->esq));
+    int total_dir = totalNO_ArvBin(&((*raiz)->dir));
+    return (total_esq + total_dir + 1);
+}
+
+int altura_ArvAVL(ArvAVL* raiz){
+    if(!raiz || !(*raiz)) return 0;
+    int alt_esq = altura_ArvBin(&((*raiz)->esq));
+    int alt_dir = altura_ArvBin(&((*raiz)->dir));
+    if(alt_esq > alt_dir) return (alt_esq + 1);
+    else return (alt_dir +1);
+}
+void preOrdem_ArvAVL(ArvAVL* raiz){
+    if(!raiz || !(*raiz)) return;
+    printf("%d\n", (*raiz)->info);
+    preOrdem_ArvBin(&((*raiz)->esq));
+    preOrdem_ArvBin(&((*raiz)->dir));
+}
+void emOrdem_ArvAVL(ArvAVL* raiz){
+    if(!raiz || !(*raiz)) return;
+    emOrdem_ArvBin(&((*raiz)->esq));
+    printf("%d\n", (*raiz)->info);
+    emOrdem_ArvBin(&((*raiz)->dir));
+}
+
+void posOrdem_ArvAVL(ArvAVL* raiz){
+    if(!raiz || !(*raiz)) return;
+    posOrdem_ArvBin(&((*raiz)->esq));
+    posOrdem_ArvBin(&((*raiz)->dir));
+    printf("%d\n", (*raiz)->info);
+}   
