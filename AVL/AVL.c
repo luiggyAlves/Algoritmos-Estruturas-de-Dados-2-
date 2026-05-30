1192ed6ed0c1ad09465797f68252d81cae4c45f8
luiggy <luiggy.alves.dev@gmail.com>
2026-05-29 20:14:30 -0400

    Implementação das rotações do TAD AVL

--- AVL/AVL.c
+++ AVL/AVL.c
@@ -102,3 +102,55 @@ int maior(int x, int y){
 }
 
 
+// ROTAÇÕES
+    // Rotações simples ocorrem quando o nó pai desbalanceado, seu filho e seu neto estão no mesmo sentido de inclinação (são todos nós da subárvore da direita, por exemplo)
+    // Rotações duplas ocorrem quando o nó pai desbalanceado e seu filho, estão inclinados no sentido inverso ao neto (o pai e filho são da subárvore da direita e o neto é de alguma subárvore esquerda, por exemplo), fazer uma rotação dupla é equivalente a fazer duas rotações simples 
+    // As rotações são aplicadas no ancestral mais próximo do nó inserido cujo fator passou a -2 ou +2
+    // Como todas as funções de rotação apenas atualizam ponteiros, a complexidade de todas é O(1)
+
+// Rotação LL ou rotação simples à direita 
+    // Ocorre quando um novo nó é inserido (chamaremos de C) na subárvore da esquerda do filho esquerdo do nó A (nó qualquer), que passa a ficar desbalanceado, precisamos fazer uma rotação à direita para balancear, o filho de A, nó intermediário B, passará a ocupar a posição de A, assim, A se torna a subárvore direita de B e a árvore volta a ficar balanceada
+
+void RotacaoLL(ArvAVL *A){
+    struct NO* B;
+    B = (*A)->esq;
+    (*A)->esq = B->dir;
+    B->dir = *A;
+    // A altura de A, quantidade de "degraus" de nós descendentes que ele tem, é a maior quantidade de descendentes de seus filhos + 1, pois conta a própria árvore
+    (*A)->alt = maior(altura_NO((*A)->esq), altura_NO((*A)->dir))+1;
+    // A altura de B, quantidade de "degraus" de nós descendentes que ele tem é igual à altura da maior de suas subárvores + 1, pois conta o próprio Nó B;
+    B->alt = maior(altura_NO(B->esq), (*A)->alt)+1;
+    *A=B; 
+}
+
+// Rotação RR ou rotação simples à esquerda
+    // Semelhantemente à rotação simples à direita (LL), a rotação simples à esquerda ocorre quando algum nó C é inserido na subárvore da direita de outro nó, que o torna desbalanceado (chamaremos o nó desbalanceado de A), assim, o nó intermediário B vira a "raiz", sendo C sua subárvore da direita e A sua subárvore da esquerda
+    // Note que no caso das duas rotações simples, os nós A, B e C ficam como se fosse uma lista, ou inclinada para a direita, ou inclinada para a esquerda
+    // rotação, basicamente a subarvore da direia de A vira a subarvore da esquerda de B, porque garantidamente é maior que A, por estar na subárvore direita de A, a subarvore esquerda de B vira A. Assim, B ocupa a posição intermediária, fazendo o balanceamento, onde sua subárvore direita é C e sua subárvore esquerda é A
+void RotacaoRR(ArvAVL* A){
+    struct NO* B;
+    B = (*A)->dir;
+    (*A)->dir = B->esq;
+    B->esq = (*A);
+    (*A)->alt = maior(alturaNO((*A)->esq), alturaNO((*A)->dir))+1;
+    B->alt = maior(alturaNO(B->dir), (*A)->alt);
+    (*A) = B;
+}
+
+
+// ROTAÇÃO LR ou rotação dupla à direita
+    // Ocorre quando um novo nó C é inserido na subárvore direita do filho esquerdo de A (chamaremos de B), sendo A o nó desbalanceado, fica como se fosse um ziguezague, por isso, precisamos fazer uma rotação dupla. Ao fazer a primeira rotação (RR), considerando B como o desbalanceado, B vira subárvore esquerda de C. Ao fazer a segunda rotação, considerando A como o desbalanceado (LL), A vira subárvore direita de C
+    // A rotação dupla pode ser implementada simplesmente com duas chamadas para rotações simples
+void RotacaoLR(ArvAVL* A){
+    RotacaoRR(&(A*)->esq);
+    RotacaoLL(A);
+}
+
+// Rotação RL ou rotação dupla à esquerda
+    // Ocorre quando o novo nó é inserido na subárvore da esquerda do filho direito de A, também fica um ziguezague, so que invertido, como se fosse um angulo inferior a 90 graus
+    // Nesse caso, o nó C (inserido ou cujo filho foi inserido) passa a ter como filho da esquerda o nó A e como filho da direita o nó B, pois primeiro ocorre uma rotação LL em A, fazendo B virar o filho à direita de C e depois uma RR em A, fazendo A virar filho à esquerda de C
+void RotacaoLR(ArvAVL* A){
+    RotacaoLL(&(A*)->dir);
+    RotacaoRR(A);
+}
+
